c329f5642424ccea61409d7dc95c22f172145ece
Nemanja1988 <nemanja_dj88@hotmail.com>
2017-08-10 22:21:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1500;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+
+    $slider.on('mouseenter', pauseSlider)
+    $slider.on('mouseleave', startSlider);
+
+    startSlider();
+
+
+
+});
+
+
+
+
+
